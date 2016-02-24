a40578c0ea9778570e4e14d94228fce30652af47
havk64 <havk.fs@gmail.com>
2016-02-23 16:55:01 -0800

    Task 0

--- js/post_a_status.js
+++ js/post_a_status.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('show-hide').addEventListener('click', function(){
     var form = document.getElementById('form');
-      form.toggle();
-    /*if (form.style.display == '') {
+      //form.toggle();
+    if (form.style.display == '') {
       form.style.display = 'block';
-    }*/
+    }
   })
 });
