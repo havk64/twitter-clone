4cf1579681f858c0a11659c05bc84fb62cc45133
Alexandro de Oliveira <havk.fs@gmail.com>
2016-03-04 19:09:12 -0800

    Testing...

--- js/post_a_status.js
+++ js/post_a_status.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
+    console.log('status');
   document.getElementById('show-hide').addEventListener('click', function(){
     var form = document.getElementById('form');
       form.toggle();
