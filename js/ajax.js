c36a575152f94241e8a0abe4d4163d4fcbfe9758
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-22 20:39:42 -0300

    Solving task 2

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,7 @@ function ajaxGet(url, onSuccess) {
         httpRequest = (window.XMLHttpRequest) ? new XMLHttpRequest: new ActiveXObject("Microsoft.XMLHTTP");
         httpRequest.onreadystatechange = function() {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-                        onSuccess(httpRequest.responseText);
+                onSuccess(httpRequest.responseText);
             }
         }
         httpRequest.open("GET", url, true );
--- js/load_more.js
+++ js/load_more.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", loadMore);
 
 function loadMore() {
     var button = document.getElementById('seeMore');
+    button.style.cursor = 'pointer';
     button.addEventListener('click', function(){
-        button.style.display = 'none';
-        ajaxGet( './statuses-1.html', function (res) {
-            var moreStatuses = document.getElementById('extrastatuses');
-            moreStatuses.innerHTML = res;
-            reset();
-            reply();
-        })
+        //button.style.display = 'none';
+        button.disabled = 'true';
+        button.style.cursor = 'not-allowed';
+        button.style.backgroundColor = 'gray';
+        setTimeout( function() {
+            ajaxGet( './statuses-1.html', function (res) {
+                var moreStatuses = document.getElementById('extrastatuses');
+                moreStatuses.innerHTML = res;
+                reset();
+                reply();
+            })
+        }, 2000 )
     })
     
 }
