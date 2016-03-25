e7f88b47d011849b57efb262916a2f1f127df611
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-25 17:12:33 -0300

    Improving weather report

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-function ajaxGet(url, onSuccess) {
+function ajaxGet(url, onSuccess) { // => Factorized function to make Ajax requests.
         httpRequest = (window.XMLHttpRequest) ? new XMLHttpRequest: new ActiveXObject("Microsoft.XMLHTTP");
         httpRequest.onreadystatechange = function() {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', request);
 
 function request() { // => Using JSONP to request a cross domain remote JSON file.
+    "use strict";
     var upWeather = document.getElementById('upweather');
     upWeather.addEventListener('click', request); // => Adding event listener in the button used to update the weather.
     var script = document.createElement('script');
@@ -11,6 +12,57 @@ function request() { // => Using JSONP to request a cross domain remote JSON fil
 }
 
 function callback(data){ // => Callback function used to wrap the result of request put it into the html element.
+    'use strict';
     var weather = document.getElementById('weather');
-    weather.innerHTML = "an "+JSON.stringify(data.current_weather)+" weather";
+    var pweather = document.getElementById('pweather');
+    var res = data.current_weather;
+    weather.innerHTML = "an "+res+" weather"; // => Puts the result into the html element.
+    switch ( res ) { // => Case Statement to change color of box background and other stuffs, according to weather.
+        case "chilly":
+            console.log("Chillyyy!!!");
+            pweather.style.backgroundColor = 'salmon';
+            break;
+        case "icy":
+            console.log("Bruuuuuu!!!");
+            pweather.style.backgroundColor = "aqua";
+            break;
+        case "sunny":
+            console.log("Yessss!!!");
+            pweather.style.backgroundColor = "Yellow";
+            break;
+        case "cloudy":
+            console.log("It's ok...");
+            pweather.style.backgroundColor = 'silver';
+            break;
+        case "snowy":
+            console.log("Oh, shhhhhhh...!!");
+            pweather.style.backgroundColor = "white";
+            pweather.style.borderStyle =  'solid';
+            pweather.style.borderWidth = '1px';
+            pweather.style.borderColor = 'silver';
+            break;
+        case "rainy":
+            console.log("Umbrella!");
+            pweather.style.backgroundColor = "gray";
+            break;
+        case "foggy":
+            console.log("Can't see anything...");
+            pweather.style.backgroundColor = "#d8d3d3";
+            break;
+        case "warm":
+            console.log("Very hot!");
+            pweather.style.backgroundColor = 'red';
+        case "beautiful":
+            console.log("That's awesome!!! :-)");
+            pweather.style.background = '#87cefa'; // => "Light Sky Blue"
+            break;
+        case "dark":
+            console.log("Welcome to the Darkness");
+            pweather.style.background = 'black';
+            pweather.style.color = 'silver';
+            break;
+        default:
+            console.log("I don't know, for sure!");
+            console.log(res);
+    }
 }
