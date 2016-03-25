d209f5a4735788386bfda7053139ffdc2ae78686
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-25 01:09:48 -0300

    Implementing reactions to weather

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,49 @@
 document.addEventListener('DOMContentLoaded', request);
 
 function request() { // => Using JSONP to request a cross domain remote JSON file.
+    "use strict";
     var upWeather = document.getElementById('upweather');
     upWeather.addEventListener('click', request); // => Adding event listener in the button used to update the weather.
     var script = document.createElement('script');
     script.type= "text/javascript";
     script.src = "https://holberton-weather-api.herokuapp.com/weather.js?jsonp_callback=callback"; 
     document.body.appendChild(script);
-    script.parentNode.removeChild(script); // => Remove the recently inserted script from the body element.
+    //script.parentNode.removeChild(script); // => Remove the recently inserted script from the body element.
 }
 
 function callback(data){ // => Callback function used to wrap the result of request put it into the html element.
+    'use strict';
     var weather = document.getElementById('weather');
-    weather.innerHTML = "an "+JSON.stringify(data.current_weather)+" weather";
+    var pweather = document.getElementById('pweather');
+    var res = data.current_weather;
+    weather.innerHTML = "an "+res+" weather"; // => Puts the result into the html element.
+    switch ( res ) { // => Case Statement to change color of box background according to weather.
+        case "chilly":
+            console.log("Chillyyy!!!");
+            pweather.style.backgroundColor = 'aqua';
+            break;
+        case "icy":
+            console.log("Bruuuuuu!!!");
+            pweather.style.backgroundColor = "green";
+            break;
+        case "sunny":
+            console.log("Yessss!!!");
+            pweather.style.backgroundColor = "Yellow";
+            break;
+        case "cloudy":
+            console.log("It's ok...");
+            pweather.style.backgroundColor = 'silver';
+            break;
+        case "snowy":
+            console.log("Oh, shhhhhhh...!!");
+            pweather.style.backgroundColor = "white";
+            break;
+        case "rainy":
+            console.log("Umbrella!");
+            pweather.style.backgroundColor = "grey";
+            break;
+        default:
+            console.log("I don't know, for sure!");
+            console.log(res);
+    }
 }
