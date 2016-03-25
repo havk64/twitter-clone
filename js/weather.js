9d487060e9101ba5f3008cc873444816b0a29db0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-24 22:08:17 -0300

    Adding feature that remove script when no more needed

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', request);
 
-function request() { // => Using JSONP in order to get a cross domain request to a remote JSON file.
+function request() { // => Using JSONP to request a cross domain remote JSON file.
     var upWeather = document.getElementById('upweather');
     upWeather.addEventListener('click', request); // => Adding event listener in the button used to update the weather.
     var script = document.createElement('script');
     script.type= "text/javascript";
     script.src = "https://holberton-weather-api.herokuapp.com/weather.js?jsonp_callback=callback"; 
     document.body.appendChild(script);
+    script.parentNode.removeChild(script);
 }
 
-function callback(data){ // => Callback function used to wrap the result of request and return the result to the html element.
+function callback(data){ // => Callback function used to wrap the result of request put it into the html element.
     var weather = document.getElementById('weather');
     weather.innerHTML = "an "+JSON.stringify(data.current_weather)+" weather";
 }
