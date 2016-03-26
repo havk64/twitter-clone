b448e4fa885da08f3f652388447e68e33dfd559d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-03-25 23:51:45 -0300

    Improving control flow behavior.js

--- js/behavior.js
+++ js/behavior.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('nav-button').addEventListener('click', function(){
     var body = document.getElementsByTagName('body')[0];
-    if (body.className == 'nav-show') {
-      body.className = '';
-    }
-    else {
-      body.className = 'nav-show';
-    }
+      body.className = (body.className == 'nav-show') ? body.className = '' : body.className = 'nav-show';
+
   })
 });
