42aa1713c3cc900fbbb95605d5f753fc3419afba
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-10 20:30:31 -0300

    Adding footer and header php files.

---
+++ views/footer.php
@@ -0,0 +1,19 @@
+<div class="clearfix"></div>
+    <footer id="footer">
+        <div class="footer-left">
+                     
+               <ul class="footer-menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">My statuses</a></li>
+                <li><a href="allusers.html">All users</a></li>
+                <li><a href="maps.html">Maps</a></li>
+                <li><a href="Impossible-Octopus-Fitness/Impossible-Octopus-Fitness.html">About</a></li>
+            </ul>
+        </div>
+        <div class="footer-center"><p>Made for a Holberton School Project, by Alexandro de Oliveira</p></div>
+        <div class="footer-right">
+            <a href="https://github.com/havk64"><img src="img/octopus-github.png" height="20px" alt="Github" /></a>
+            <a href="https://twitter.com/havk64"><img src="img/twitter.png" height="20px" alt="Github" /></a>
+            <a href="https://www.linkedin.com/in/alexandroliveira"><img src="img/linkedin.png" height="25px" alt="Github" /></a>
+        </div>
+    </footer> 
