f7f423592bc0bf717a0263397d506bc75a9aadb5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-10 21:02:35 -0300

    Replacing aside with php partial, login.php

--- login.php
+++ login.php
@@ -35,18 +35,9 @@ $about = "Impossible-Octopus-Fitness/Impossible-Octopus-Fitness.html";
                 </form>
             </section>
         </article>
-        <aside>
-            <section>
-                <h3 class="title">Dennis Ritchie</h3>
-                <img src="img/ritchie.jpg" alt="Denns Ritchie">
-                <p>Father of C language and Unix Operating System</p><p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo harum dolorem quasi, officia earum velit libero fuga culpa quaerat sit in nam assumenda impedit tenetur sapiente suscipit architecto nostrum possimus.</p>
-            </section>
-            <section>
-                <h3 class="title">Alexandro de Oliveira</h3>
-                <img src="img/Alexandro%20de%20Oliveira.jpg" alt="Alexandro de Oliveira">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore fugiat officia quam quis vero, ipsum reiciendis et debitis a harum. Nemo distinctio beatae quis delectus autem obcaecati vero hic dolore!</p>
-            </section>
-        </aside>
+        
+        <?php include("views/aside.php");?> 
+        
     </main>
     <div class="clearfix"></div>
     
