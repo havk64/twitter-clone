bdf7ffa6668755d9589018c61eb1873c17fb9446
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-10 21:46:21 -0300

    Including login information to all pages

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<?php //Array of registered users.
-$users = [
-    array("id" => 1, "login" => "user1", "password" => "password1", "full_name" => "User 1"),
-    array("id" => 2, "login" => "user2", "password" => "password2", "full_name" => "User 2"),
-    array("id" => 3, "login" => "user3", "password" => "password3", "full_name" => "User 3"),
-  ];
-?> <!-- / End of Array. -->
- <!-- ============================================== -->
- 
- <?php //Defining the function to check the user information.
-  function userExists($login, $password, $users) 
+  <?php //Defining the function to check the user information.
+
+    include_once "models/user.php"; //Including information about registered users.
+
+    function userExists($login, $password, $users) 
   { // Function to check if the user is in the database.
       $ret = '';
       foreach ($users as $user)  {
