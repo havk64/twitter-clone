ed0129fec7af3b173642c4074ba8776b7981200f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-12 02:53:56 -0300

    Fixing login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if($cookie){
     $current_user = $cookie;
 }
 $home = "index.php";
-$showhide = showHide($current_user);
+$logged = logged($current_user);
 $allusers = "allusers.php";
 $maps = "maps.php";
 $about = "Impossible-Octopus-Fitness/Impossible-Octopus-Fitness.php";
