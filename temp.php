f78ed0eb01a7056e89c9fb4d344f1902bdff3dc1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-15 20:27:59 -0300

    Adding Compass files + Test files to Css

---
+++ temp.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Css test</title>
+    <link rel="stylesheet" href="css/test.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <!--Begin main container-->
+
+        <?php include("views/header.php"); ?>
+
+            <main>
+
+            </main>
+
+        <?php include("views/footer.php"); ?>
+
+    </div>
+</body>
+
+</html>
