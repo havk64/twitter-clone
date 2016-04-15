62b77ec332d5f02e74dd83abad38d7a96aa554a2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-15 20:21:52 -0300

    Updating header partial, preparing to rebuild all Css with Sass/Compass

--- views/header.php
+++ views/header.php
@@ -5,24 +5,28 @@
             <p>Likable Stats</p>
         </div>
 
-        <ul class="top-menu">
-           <li><a href="/login.php">Login</a></li>
-            <li><a href="<?php echo($logged['profile']);?>">Edit my profile</a></li>
-            <li><a href="/logout.php">Logout</a></li>
-        </ul>
-
-       <!-- <div class="clearfix"></div> replacing for use flexbox -->
+        
 
         <div class="outer-menu-container">
             <ul class="main-menu">
                 <li><a href="<?php echo($home);?>">Home</a></li>
-                <li style="display:<?php echo($logged['hide']);?>"><a href="<?php echo($logged['mystatuses']);?>">My statuses</a></li>
+                <li style="display:<?php echo($logged['hide']);?>">
+			<a href="<?php echo($logged['mystatuses']);?>">My statuses</a>
+		</li>
                 <li><a href="<?php echo ($allusers)?>">All users</a></li>
                 <li><a href="<?php echo($maps)?>">Maps</a></li>
                 <li><a href="<?php echo($about)?>">About</a></li>
             </ul>
         </div>
-        <button id="all-images" type="submit" value="Send">All images on this pages</button>
+        
+        <div>
+            <ul class="top-menu">
+               <li><a href="/login.php">Login</a></li>
+                <li><a href="<?php echo($logged['profile']);?>">Edit my profile</a></li>
+                <li><a href="/logout.php">Logout</a></li>
+            </ul>
+        </div>
+       <!--  <button id="all-images" type="submit" value="Send">All images on this pages</button> -->
         
         
 
