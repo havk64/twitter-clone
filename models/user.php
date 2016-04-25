d7ea20b90eded9479e01f62f0edab9948cf05de4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-04-25 14:42:46 -0300

    Task 0 completed, replacing php array with json file

--- models/user.php
+++ models/user.php
@@ -1,19 +1,22 @@
 <?php
 function listUsers(){
-    
+    $contents = file_get_contents('data/users.json');
+    $parsed = json_decode($contents, true);
+    return $parsed;
 }
+$users = listUsers();
 ?>
 <?php //Defining the function to check the user information.
 function userExists($login, $password, $users) 
   { // Function to check if the user is in the database.
-      $ret = '';
-      foreach ($users as $user)  {
-          if($login == $user['login'] && $password == $user['password']){ //If user login succesfull. Return it outside of the loop.
+    $ret = '';
+    foreach ($users as $user)  {
+        if($login == $user['login'] && $password == $user['password']){ //If user login succesfull. Return it outside of the loop.
             $ret = $user; //Can't return inside of loop. Assigning to a variable to return it later.
-          }
-      }
-        return $ret; //Returning the variable.
-  } 
+        }
+    }
+    return $ret; //Returning the variable.
+} 
 
 function hasCookie($login, $users)
 {
